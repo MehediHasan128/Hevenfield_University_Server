253f3805687002566079cc261df96c9a8a061d0a
MehediHasan128 <mehedihasan12926@gmail.com>
2025-01-02 14:41:40 +0600

    add not found middlware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import globalErrorHandler from './app/middlwares/globalErrorHandler';
+import notFound from './app/middlwares/notFound';
 
 const app: Application = express();
 
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
 });
 
 // User middlwares
+
+// Add global error handler middlwares
 app.use(globalErrorHandler as (err: any, req: Request, res: Response, next: NextFunction) => void);
+// Add not found middlwares
+app.use(notFound as (req: Request, res: Response, next: NextFunction) => void);
 
 export default app;
