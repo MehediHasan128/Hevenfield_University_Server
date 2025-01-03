4e93ac6ed6ba9982f57bb530844564d897f5627e
MehediHasan128 <mehedihasan12926@gmail.com>
2025-01-03 23:09:46 +0600

    add student router in application routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.router";
+import { StudentRoutes } from "../modules/student/student.router";
 
 const router = Router();
 
 const moduleRoutes = [
     {
-        path: '',
+        path: '/users',
         routes: UserRoutes
-    }
+    },
+    {
+        path: '/students',
+        routes: StudentRoutes
+    },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.routes));
 
-export default router
+export default router;
