7b613a0de8c63d215097812f99ad6d3d411ce21b
MehediHasan128 <mehedihasan12926@gmail.com>
2025-01-18 12:04:55 +0600

    change user password

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -50,7 +50,23 @@ const loginUser = async(payload: TUserLogin) => {
 };
 
 const changeUserPassword = async(userData: JwtPayload, payload: TChangePassword) => {
-    console.log(payload, userData);
+
+    // check if the user is exist on database
+    const isUserExist = await User.findOne({email: userData?.userEmail}, {_id: 0, password: 1});
+    if(!isUserExist){
+        throw new AppError(httpStatus.NOT_FOUND, 'User not found!')
+    }
+    
+    // Check the old password is correct or not
+    const isOldPasswordIsCorrect = await bcrypt.compare(payload?.oldPassword, isUserExist?.password);
+    if(!isOldPasswordIsCorrect){
+        throw new AppError(httpStatus.BAD_REQUEST, 'Incorrect Password!');
+    }
+
+    // If old password is correct then bcrypt the new password and update
+    const newPassword = await bcrypt.hash(payload?.newPassword, Number(config.bcrypt_salt_round));
+
+    await User.findOneAndUpdate({email: userData?.userEmail, role: userData?.userRole}, {password: newPassword, needPasswordChange: false, passwordChangeAt: new Date()}, {new: true}).select('-password');
 }
 
 export const AuthServices = {
